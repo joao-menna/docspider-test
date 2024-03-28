461c365fb99faf7e483ad113cc2164a71313d198
joao-menna <joao.menna1907@gmail.com>
2024-03-28 20:23:37 -0300

    corrigido mes vindo errado

--- frontend-html/assets/js/helpers.js
+++ frontend-html/assets/js/helpers.js
@@ -11,7 +11,7 @@ function formatDate(dateTime) {
 
   const date = new Date(dateTime)
   const day = date.getDate().toString()
-  const month = date.getMonth().toString()
+  const month = (date.getMonth() + 1).toString()
   const year = date.getFullYear()
   const hour = date.getHours().toString()
   const minute = date.getMinutes().toString()
